cd67a3bdad4be537bd17c6aac38f5fff68fca46f
SUBHANGI-DHASMANA <91680044+SUBHANGI-DHASMANA@users.noreply.github.com>
2023-06-05 13:29:01 +0530

    Added 3 files

---
+++ dsa sheet solution/Array/11- maximum subarray sum/brute.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i; j<nums.size(); j++){
+                int sum = 0;
+                for(int k=i; k<=j; k++){
+                    sum += nums[k];
+                }
+                maxi = max(sum, maxi);
+            }
+        }
+        return maxi;
+    }
+};
